7c582f8395d15949822bfa3c083349a2b6476c5d
Administrator <Administrator@192.168.20.59>
2019-09-24 09:10:27 +0200

    24/09/2019

--- src/s066bis2/Main.java
+++ src/s066bis2/Main.java
@@ -1,55 +1,26 @@
 package s066bis2;
-
 public class Main {
-	public static void main(String[] args) {
-		Persona[] persone = new Persona[2];
-		persone[0]= new Dipendente();
-		persone[1]= new Consulente();
-		
-	}
-}
 
-//public class Main {
-//	public static void main(String[] args) {
-//		Vehicle[] vehicles = new Vehicle[3];
-//		vehicles[0] = new Car();
-//		vehicles[1] = new Bus();
-//		vehicles[2] = new MotorBike();
-//
-//		boolean rightDirection = true;
-//		for (Vehicle vehicle : vehicles) {
-//			// (2) bike: call the specific steer w/angle method
-//			if (vehicle instanceof MotorBike) {
-//				MotorBike bike = (MotorBike) vehicle;
-//
-//			} else {
-//				vehicle.steer(rightDirection);
-//				rightDirection = !rightDirection;
-//			}
-//
-//			Conditioning[] conditionings = new Conditioning[2];
-//			conditionings[0] = new Bus();
-//			conditionings[1] = new Car();
-//
-//			int temperature = 20;
-//			for (Conditioning conditioning : conditionings) {
-//				conditioning.setTemperature(temperature);
-//				temperature += 1;
-//
-//			}
-//			// (1) steer the car in conditionings
-//			for (Conditioning conditioning : conditionings) {
-//				if (conditioning instanceof Car) {
-//					System.out.println("This is a car ->");
-//					Car car = (Car) conditioning;
-//					car.steer(true);
-//				} else {
-//					System.out.println("This is not a car");
-//				}
-//
-//			}
-//
-//		}
-//
-//	}
-//}
+    public static void main(String[] args) {
+        Person[] persons = new Person[3];
+        persons[0] = new Consultant("Tom", "Jones", 1000);
+        persons[1] = new Employee("Jim", "Smith", 3000);
+        persons[2] = new Consultant("Bob", "Korn", 800);
+
+        for (Person person : persons) {
+            System.out.println(person);
+        }
+
+        for (Person person : persons) {
+            if(person instanceof Consultant) {
+                Consultant c = (Consultant)person;
+                System.out.println(c.getDaily());
+
+            } else if(person instanceof Employee) {
+                Employee e = (Employee)person;
+                System.out.println(e.getSalary());
+            }
+
+        }
+    }
+}
