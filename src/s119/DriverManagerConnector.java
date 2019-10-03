691fc8ecda24f7b5d33c6deaae0bf17e7fd99c03
Administrator <Administrator@192.168.20.59>
2019-10-03 15:20:05 +0200

    03/10/2019

--- src/s119/DriverManagerConnector.java
+++ src/s119/DriverManagerConnector.java
@@ -5,16 +5,23 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DriverManagerConnector {
-    private static final String URL = "jdbc:oracle:thin:@localhost:1521/xepdb1"; //sinonimo di localhost è 127.0.0.1; il numero della porta è 1521
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521/xe"; //sinonimo di localhost è 127.0.0.1; il numero della porta è 1521; xe è il server di Oracle
     private static final String USER = "me";
     private static final String PASSWORD = "password";
 
     public static void main(String[] args) {
         try {
-            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD);
+            Connection conn = DriverManager.getConnection(URL, USER, PASSWORD); //Fondamentale avere jdbs di oracle
+            
+//            String user = conn.getSchema();
+//            if (user==null) {
+//            	user=conn.getCatalog();
+//            }
             System.out.println("Connected as " + conn.getSchema());
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 }
+
+
